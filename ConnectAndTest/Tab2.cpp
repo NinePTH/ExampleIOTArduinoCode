08ba0dd8bca820d0f6ec4fdf031c4ff8442a6052
Patihan Prasongphol <105841902+NinePTH@users.noreply.github.com>
2022-09-07 23:26:20 +0700

    Create Tab2.cpp

---
+++ ConnectAndTest/Tab2.cpp
@@ -0,0 +1,10 @@
+#define BLYNK_TEMPLATE_ID "TMPL8Ms5jeH_"
+#define BLYNK_DEVICE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "OO3idLQwEksycpX_OOCect9lDUklUXsE"
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "AndroidAPED3F";
+char pass[] = "ckkz7752";
